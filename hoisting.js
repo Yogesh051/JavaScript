48196e2f3998c389383bd508e0edf3de839197d0
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2024-11-16 00:15:57 +0530

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,65 @@
+// function greetMe(greet , fullname){
+//     console.log("hello", fullname);
+//     greet();
+// }
+
+
+// function greet(){
+//     console.log("Good evening");
+// }
+// greetMe(greet, "Yogesh"); 
+
+
+
+// function solve(number){
+//     return function(number){
+//         return number*number;
+//     }
+// }
+
+// let ans= solve(5);
+// let finalAns= ans(10);
+// console.log(finalAns);
+//solve function khud ek function return kar rha ha
+
+
+
+//array of functions 0 1 2 index
+// const arr= [
+//     function(a,b){
+//         return a+b;
+//     },
+//     function(a,b){
+//         return a-b;
+//     },
+//     function(a,b){
+//         return a*b;
+//     }
+// ]
+
+// let first = arr[0];
+// let ans = first(5,10);
+// console.log(ans);
+
+
+
+// let obj ={
+//     age:25,
+//     wt:55,
+//     ht:170,
+//     greet: ()=>{
+//         console.log("Hello Dunia");
+//     }
+// }
+// console.log(obj.age);
+// obj.greet(); 
+
+
+
+// var greet;
+// console.log(greet);
+
+// var greet= function(){
+//     console.log("Namaste dubai");
+// }
+//function ki hoistiong nhi hote ha but function ko var nam ki variable ma store kr phir uski hoisting kre undefined islia aya kyuki var to upar declare ho gya but uski value nicha he rh gye

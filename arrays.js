0c071c52a5ab93c180a5ff888a1dd77d5723b881
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2024-11-16 00:16:19 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,2 @@
+let arr = ["Love", "Babbar", true, 35];
+console.log(arr[1]);
---
+++ obj.js
@@ -0,0 +1,12 @@
+let obj = {
+    name: "Yogesh",
+    age: 21,
+    weight : 56,
+    height : 170,
+
+    greet: function(){
+        console.log("Hello Everyone");
+    }
+};
+obj.greet()
+console.log(obj);
